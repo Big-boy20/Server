371d9e5e357d2ead639d6e0ba45a114336934492
Dima <dimon18112000@gmail.com>
2022-02-20 18:44:46 +0200

    Task

---
+++ Task1sky2.js
@@ -0,0 +1,17 @@
+ const createUser = function (name, age) {
+     return{
+         name,
+         age,
+         toString: function(){
+             return `${this.name} is ${this.age} y.o.`;
+         },
+     }
+ } 
+const users =[
+    createUser("John", 21),
+    createUser("Tory", 18),
+    createUser("Kate", 23),
+    createUser("Pete", 22),
+]
+users.sort((a, b)=> a.age>b.age?1:-1);
+console.log(users)
